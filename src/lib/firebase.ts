7c93bbd17ef5ecd9fcc08be183ff182b76cbc04b
vanshikaChhikaraGit <vanshikachhikaraofficial@gmail.com>
2025-05-09 14:48:38 +0530

    modified meeting component

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,15 +10,16 @@ import { error } from "console";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyClb83YmQ8EIWUE4DwIHqcHrfPyXLt0xYg",
-  authDomain: "dionysusmeeting.firebaseapp.com",
-  projectId: "dionysusmeeting",
-  storageBucket: "dionysusmeeting.firebasestorage.app",
-  messagingSenderId: "206503368966",
-  appId: "1:206503368966:web:a4fe2abcd5a811457ca687",
-  measurementId: "G-79R2GQV9QT"
-};
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  };
 
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app)
